2f1370ebe60aac4557657f87d711af250811df5f
Petros <petros-d@github.com>
2020-05-18 22:21:59 +0100

    Print OpenID token with app roles

--- shinyapps/app.r
+++ shinyapps/app.r
@@ -4,9 +4,9 @@ library(AzureAuth)
 options(shiny.port=5000)
 
 # parameters -- set the redirect URI for the app to 'Public Client / http://localhost:8100'
-resource <- "https://graph.microsoft.com/"
+resource <- c("https://graph.microsoft.com/.default", "openid", "profile", "offline_access", "email" )
 tenant <- "xxxxxxx-254d-43bf-98df-xxxxxxxxx"
-app <- "xxxxxxx-ef19-4bd3-9e13-xxxxxxxxx"
+app <- "xxxxxxx-a844-4a4b-b48f-xxxxxxxx"
 
 
 has_auth_code <- function(params)
@@ -16,14 +16,15 @@ has_auth_code <- function(params)
 
 ui <- fluidPage(
     # Your regular UI goes here, for when everything is properly auth'd
-    verbatimTextOutput("token")
+    verbatimTextOutput("token"),
+    verbatimTextOutput("id")
 )
 
 ui_func <- function(req)
 {
     if(!has_auth_code(parseQueryString(req$QUERY_STRING)))
     {
-        url <- build_authorization_uri(resource, tenant, app, redirect_uri="http://localhost:5000")
+        url <- build_authorization_uri(resource, tenant, app, redirect_uri="http://localhost:5000", version=2)
         redirect <- sprintf("location.replace(\"%s\");", url)
         tags$script(HTML(redirect))
     }
@@ -36,15 +37,11 @@ server <- function(input, output, session)
     if(!has_auth_code(params))
         return()
 
-    token <- get_azure_token(resource, tenant, app, authorize_args=list(redirect_uri="http://localhost:5000"),
-                             use_cache=FALSE, auth_code=params$code, auth_type="authorization_code")
-
-    # When specifying the password as below, the authentication flow works as client_credential:                         
-    #token <- get_azure_token(c("https://graph.microsoft.com/"),tenant,password="/zY.uiYOA1msV=JaBdZT4YNj5tY_0RVP",
-    #                    app,version=2,authorize_args=list(redirect_uri="http://localhost:5000"),use_cache=FALSE, 
-    #                    auth_code=params$code)
+    token <- get_azure_token(resource="openid", tenant, app, authorize_args=list(redirect_uri="http://localhost:5000"),
+                             use_cache=FALSE, version=2, auth_code=params$code)
 
     output$token <- renderPrint(token)
+    output$id <- renderPrint(decode_jwt(token, "id"))
 }
 
 shinyApp(ui_func, server)
